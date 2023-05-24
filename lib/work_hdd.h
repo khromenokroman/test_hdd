28e6d0ed76f15e2761a250fe66f5ce41665e7a5c
khromenokroman <roma55592@yandex.ru>
2023-05-24 22:09:16 +0200

    add&

--- lib/work_hdd.h
+++ lib/work_hdd.h
@@ -14,9 +14,9 @@ private:
     explicit Device(std::string &file_name);         // open file
 
 public:
-    Device operator=(Device &&dev) = delete; // =move del
+    Device &operator=(Device &&dev) = delete; // =move del
     Device(const Device &&dev) = delete;    // move del
-    Device operator=(Device &dev) = delete; // = del
+    Device &operator=(Device &dev) = delete; // = del
     Device(const Device &dev) = delete;     // copy del
 
     void write_file();                                 // write in file
@@ -29,9 +29,9 @@ class My_error final : public std::exception
 public:
     explicit My_error(const std::string &&message) : message{message} {}
     const char *what() const noexcept override { return message.c_str(); } // message
-    My_error operator=(My_error &&err) = delete;                            // =move del
+    My_error &operator=(My_error &&err) = delete;                            // =move del
     My_error(const My_error &&err) = delete;                               // move del
-    My_error operator=(My_error &err) = delete;                            // = del
+    My_error &operator=(My_error &err) = delete;                            // = del
     My_error(const My_error &err) = delete;                                // copy del
 private:
     std::string message; // message
