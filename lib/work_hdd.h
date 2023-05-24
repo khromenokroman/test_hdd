3ea7c09c4d8156208048013a1e06aeada11f6fb0
khromenokroman <roma55592@yandex.ru>
2023-05-24 22:14:08 +0200

    add &

--- lib/work_hdd.h
+++ lib/work_hdd.h
@@ -9,15 +9,15 @@ private:
     const size_t buffer_size = 4096; // buffer
     size_t data_write;               // size data write
 
-    void open_file(std::string &file_name); // open file
-    void create_buffer();                   // create buf
-    explicit Device(std::string &file_name);         // open file
+    void open_file(std::string &file_name);  // open file
+    void create_buffer();                    // create buf
+    explicit Device(std::string &file_name); // open file
 
 public:
     Device &operator=(Device &&dev) = delete; // =move del
-    Device(const Device &&dev) = delete;    // move del
-    Device &operator=(Device &dev) = delete; // = del
-    Device(const Device &dev) = delete;     // copy del
+    Device(const Device &&dev) = delete;      // move del
+    Device &operator=(Device &dev) = delete;  // = del
+    Device(const Device &dev) = delete;       // copy del
 
     void write_file();                                 // write in file
     Device(std::string &file_name, size_t data_write); // buffer
@@ -29,9 +29,9 @@ class My_error final : public std::exception
 public:
     explicit My_error(const std::string &&message) : message{message} {}
     const char *what() const noexcept override { return message.c_str(); } // message
-    My_error &operator=(My_error &&err) = delete;                            // =move del
+    My_error &operator=(My_error &&err) = delete;                          // =move del
     My_error(const My_error &&err) = delete;                               // move del
-    My_error &operator=(My_error &err) = delete;                            // = del
+    My_error &operator=(My_error &err) = delete;                           // = del
     My_error(const My_error &err) = delete;                                // copy del
 private:
     std::string message; // message
