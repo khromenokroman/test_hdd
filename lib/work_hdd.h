db7d59bb437a7ddccfb279048bb6b6313f9ef5dd
khromenokroman <roma55592@yandex.ru>
2023-05-25 14:10:32 +0200

    remove include

--- lib/work_hdd.h
+++ lib/work_hdd.h
@@ -1,25 +1,20 @@
-#pragma once
-#include <iostream>
-#include <memory>
-
 class Device final // not member
 {
 private:
-    int fd; // file descriptor
-    std::unique_ptr<char[]> uniqPtrBuffer;
-    char *buf = nullptr;             // bufer
-    const size_t buffer_size = 4096; // buffer
-    size_t data_write;               // size data write
+    int fd;                                 // file descriptor
+    std::unique_ptr<char[]> buf;            // bufer
+    static const size_t buffer_size = 4096; // buffer
+    size_t data_write;                      // size data write
 
     void open_file(std::string &file_name);  // open file
     void create_buffer();                    // create buf
     explicit Device(std::string &file_name); // open file
 
 public:
-    Device &operator=(Device &&dev) = delete; // =move del
-    Device(const Device &&dev) = delete;      // move del
-    Device &operator=(Device &dev) = delete;  // = del
-    Device(const Device &dev) = delete;       // copy del
+    Device &operator=(Device &&dev) = default; // =move del
+    // Device(const Device &&dev) = delete;      // move del
+    // Device &operator=(Device &dev) = delete;  // = del
+    // Device(const Device &dev) = delete;       // copy del
 
     void write_file();                                 // write in file
     Device(std::string &file_name, size_t data_write); // buffer
