798a62a7c8b56e410c0f5ec2cff33a8d2e4570d9
khromenokroman <roma55592@yandex.ru>
2023-05-25 15:32:01 +0200

    remove

--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,10 @@
 int main()
 {
     std::string file_name = "data";
-    std::string file_name2 = "data2";
     try
     {
         Device nvme(file_name, 2);
         nvme.write_file();
-
-        // Device a(file_name2, 2);
-        // a.write_file();
-        
-        // a = std::move(nvme);
 
     }
     catch (My_error &ex)
